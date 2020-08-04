71461b5a25898c2ee1da4720ffcbf17ba532bc2a
akif-ih <akif@inventhub.io>
2020-08-04 18:11:56 +0500

    Updates

---
+++ precache-manifest.6546ac73d7d18e539a507527135f4cf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4571b0917b4dc26c165c0295ed219d58",
+    "url": "/countries-finder/index.html"
+  },
+  {
+    "revision": "3bcf2e2d5422431285ee",
+    "url": "/countries-finder/static/css/main.be18fcab.chunk.css"
+  },
+  {
+    "revision": "315aee0327789354a6e1",
+    "url": "/countries-finder/static/js/2.0d8c670d.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/countries-finder/static/js/2.0d8c670d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bcf2e2d5422431285ee",
+    "url": "/countries-finder/static/js/main.67c4e8d7.chunk.js"
+  },
+  {
+    "revision": "40d81c0378020e968d01",
+    "url": "/countries-finder/static/js/runtime-main.85021951.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.92f4c51f935a921ead3f5d29ac92e044.js"
+  "/countries-finder/precache-manifest.6546ac73d7d18e539a507527135f4cf9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/countries-finder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
