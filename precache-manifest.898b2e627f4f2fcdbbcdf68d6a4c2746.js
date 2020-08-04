b552ba6b1e077e8b007c746137f3213221523b8c
akif-ih <akif@inventhub.io>
2020-08-04 18:00:58 +0500

    Updates

---
+++ precache-manifest.898b2e627f4f2fcdbbcdf68d6a4c2746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92870a99b3369ada418377caf9e5893",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a61a16297404410a9921",
+    "url": "/static/css/main.be18fcab.chunk.css"
+  },
+  {
+    "revision": "315aee0327789354a6e1",
+    "url": "/static/js/2.0d8c670d.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/static/js/2.0d8c670d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61a16297404410a9921",
+    "url": "/static/js/main.09a71119.chunk.js"
+  },
+  {
+    "revision": "425b33d36f0420e00f48",
+    "url": "/static/js/runtime-main.8f8cf916.js"
+  }
+]);
